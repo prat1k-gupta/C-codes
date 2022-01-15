6232092ac11720c01738fcb0c12e1d8949d28a21
prat1k-gupta <pratikgupta9773@gmail.com>
2022-01-16 01:20:34 +0530

    working on lifo

--- stack/LIFO.c
+++ stack/LIFO.c
@@ -1,37 +1,51 @@
 #include<stdio.h>
-int top=-1;
-char stack[100],stack2[100];
-void push(int n);
-void pop(char stack[]);
+#include<string.h>
+int top=-1,l=-1,star=0,alpha=0;
+char st[100];
+void push(char c);
+void pop(char z);
 int main(){
-    char n;
-    do{
-    printf("enter your inputs:");
-    scanf("%c",&n);
-    if(n >='a' && n<='z'){
-        push(n);
+    int j=0;
+    char arr[100];
+    printf("input elements");
+    scanf("%[^\n]s",&arr);
+    int size = strlen(arr);
+    while(j<size){
+        if(arr[j]>='A' && arr[j]<='Z'){
+            top++;
+            printf("value of top in if is %d\n",top);
+        }
+        else{
+            printf("value of top in else is %d\n",top);
+            if(arr[top]!='*'){
+                pop(arr[top]);
+                top++;
+            }
+            else{
+                alpha++;
+                star++;
+                printf("no. of star are: %d",star);
+                printf("no. of alpha are: %d",alpha);
+                int temp=top;
+                temp=temp-star-alpha;
+                pop(arr[temp]);
+
+            }
+        }
+        j++;
     }
-    else{
-        pop(stack);
-    }
-    }while(n!='x');
-     for (int i = 0; i < 5; i++)
-    {
-        printf("elements in stack are %c\n", stack[i]);
-    }
-     for (int i = 0; i < 5; i++)
-    {
-        printf("elements in stack are %c\n", stack2[i]);
-    }
+    display();
     return 0;
 }
-void push(int n){
-    top++;
-    stack[top]=n;
+void pop(char z){
+        l++;
+        st[l]=z;
+        printf("pop[%c]\n",st[l]); 
 }
-void pop(char stack[]){
-    int i=0;
-    stack2[i]=stack[top];
-    i++;
-    top--;
+void display(){
+    for (int i = 0; i < strlen(st); i++)
+    {
+        printf("%c",st[i]);
+    }
+    
 }
